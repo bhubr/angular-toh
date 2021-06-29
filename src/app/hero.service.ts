acdfa2561e839cf8815fc0bedc6632148bfe2075
Benoît Hubert <benoithubert@gmail.com>
2021-06-30 00:44:13 +0200

    Step 6b. Get Data from a Server (Incomplete: Read only)

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,25 +1,53 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'https://my-json-server.typicode.com/bhubr/angular-toh/heroes';
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(_ => this.messageService.add('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.messageService.add(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
   getHero(heroId: Number): Observable<Hero> {
-    const hero = HEROES.find(h => h.id === heroId)!;
-    return of(hero);
+    return this.http.get<Hero>(`${this.heroesUrl}/${heroId}`).pipe(
+      tap(_ => this.messageService.add(`fetched hero id=${heroId}`)),
+      catchError(this.handleError<Hero>(`getHero id=${heroId}`))
+    );
   }
 }
